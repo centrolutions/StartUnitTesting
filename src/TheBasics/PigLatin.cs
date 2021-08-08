58df4eb936795bc76fe5c840a9f7aa529798966c
Jason Williams <jason@centrolutions.com>
2021-08-08 17:10:46 -0500

    Solved PigLatin

--- src/TheBasics/PigLatin.cs
+++ src/TheBasics/PigLatin.cs
@@ -1,10 +1,85 @@
-﻿namespace TheBasics
+﻿using System.Collections.Generic;
+
+namespace TheBasics
 {
     public class PigLatin
     {
         public string Translate(string sentence)
         {
-            return sentence;
+            List<string> translatedWords = TranslateAllWords(sentence);
+            return CreateSentenceFromWords(translatedWords);
+        }
+
+        private static string CreateSentenceFromWords(List<string> translatedWords)
+        {
+            return string.Join(' ', translatedWords.ToArray());
+        }
+
+        private List<string> TranslateAllWords(string sentence)
+        {
+            var translatedWords = new List<string>();
+            var words = sentence.Split(' ');
+            foreach (var word in words)
+            {
+                translatedWords.Add(TranslateWord(word));
+            }
+
+            return translatedWords;
+        }
+
+        private string TranslateWord(string word)
+        {
+            var wasCapitalized = StartsWithUpperCase(word);
+            var endingPunctuation = GetPunctuationFromEnd(word);
+            if (endingPunctuation != default)
+                word = word.Substring(0, word.Length - 1);
+
+            if (StartsWithVowel(word))
+                return word + "way";
+
+            word = word.ToLower();
+            var consonantsAtEnd = MoveConsonantsToEnd(word);
+            var newWord = consonantsAtEnd + "ay";
+
+            if (wasCapitalized)
+                newWord = newWord.Substring(0, 1).ToUpper() + newWord.Substring(1);
+
+            if (endingPunctuation != default)
+                newWord += endingPunctuation;
+
+            return newWord;
+        }
+
+        private string MoveConsonantsToEnd(string word)
+        {
+            if (StartsWithVowel(word))
+                return word;
+
+            var newWord = word.Substring(1) + word.Substring(0, 1);
+            return MoveConsonantsToEnd(newWord);
+        }
+
+        private bool StartsWithVowel(string word)
+        {
+            word = word.ToLower();
+            return word.StartsWith('a') || word.StartsWith('e')
+                || word.StartsWith('i') || word.StartsWith('o')
+                || word.StartsWith('u');
+        }
+
+        private bool StartsWithUpperCase(string word)
+        {
+            var firstLetter = word[0];
+            return char.IsUpper(firstLetter);
+        }
+
+        private char GetPunctuationFromEnd(string word)
+        {
+            var lastCharacter = word[word.Length - 1];
+            if (char.IsLetter(lastCharacter))
+                return default;
+
+            return lastCharacter;
         }
     }
 }
---
+++ src/TheBasicsTests/PigLatinTests.cs
@@ -0,0 +1,84 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TheBasics;
+
+namespace TheBasicsTests
+{
+    public class PigLatinTests
+    {
+        PigLatin _Sut;
+
+        [SetUp]
+        public void Setup()
+        {
+            _Sut = new PigLatin();
+        }
+
+        [TestCase("over", "overway")]
+        [TestCase("away", "awayway")]
+        [TestCase("a", "away")]
+        [TestCase("it", "itway")]
+        public void Translate_WayIsAddedToEndOfWord_WhenWordStartsWithVowel(string input, string expected)
+        {
+            var result = _Sut.Translate(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("fox", "oxfay")]
+        [TestCase("jumped", "umpedjay")]
+        [TestCase("dog", "ogday")]
+        public void Translate_ConsonantIsMovedToEndAndAyIsAddedToEnd_WhenWordStartsWithConsonant(string input, string expected)
+        {
+            var result = _Sut.Translate(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestCase("quick", "uickqay")]
+        [TestCase("brown", "ownbray")]
+        [TestCase("the", "ethay")]
+        public void Translate_AllConsonantsAreMovedToEndAndAyIsAddedToEnd_WhenWordStartsWithMultipleConsonants(string input, string expected)
+        {
+            var result = _Sut.Translate(input);
+
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void Translate_TranslatesEntireSentence_WhenMoreThanOneWordIsUsed()
+        {
+            var result = _Sut.Translate("the quick brown fox jumped over the dog");
+
+            Assert.AreEqual("ethay uickqay ownbray oxfay umpedjay overway ethay ogday", result);
+        }
+
+        [Test]
+        public void Translate_TranslatesWord_WhenWordHasCapitolLettersInIt()
+        {
+            var result = _Sut.Translate("The");
+
+            Assert.AreEqual("Ethay", result);
+        }
+
+        [Test]
+        public void Translate_TranslatesWordThatStartsWithVowel_WhenWordHasCapitalLetter()
+        {
+            var result = _Sut.Translate("Over");
+
+            Assert.AreEqual("Overway", result);
+        }
+
+        [Test]
+        public void Translate_KeepsPunctuationAtEndOfWord_WhenWordHasCommaAtEnd()
+        {
+            var result = _Sut.Translate("quick,");
+
+            Assert.AreEqual("uickqay,", result);
+        }
+    }
+}
